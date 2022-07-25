5d74db2bdd5ff2391991ecd90b35a81b35615cad
mateusCoder <mateus.moraes0507@gmail.com>
2022-07-25 11:29:30 -0300

    Created Project

---
+++ tripshare/src/main/java/com/devs/tripshare/TripShareApplication.java
@@ -0,0 +1,13 @@
+package com.devs.tripshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripShareApplication.class, args);
+	}
+
+}
---
+++ tripshare/src/test/java/com/devs/tripshare/TripShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.tripshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
