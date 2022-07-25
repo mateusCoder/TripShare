69dc590e45b5c88f422b24438de841d4b167d202
Yhonathan Pavan <yhonathannpavan@gmail.com>
2022-07-25 17:43:28 -0300

    Adicionado repository e controller

---
+++ tripshare/src/main/java/com/devs/tripshare/controllers/RideController.java
@@ -0,0 +1,9 @@
+package com.devs.tripshare.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tripshare/people")
+public class RideController {
+}
