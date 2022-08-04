e966b68a67ab780e4e2108393464b62f8504d2a6
mateusCoder <mateus.moraes0507@gmail.com>
2022-08-04 12:31:29 -0300

    Add Update Return of Trip Annotation

--- tripshare/src/main/java/com/devs/tripshare/config/annotations/trips/UpdateTripDocConfig.java
+++ tripshare/src/main/java/com/devs/tripshare/config/annotations/trips/UpdateTripDocConfig.java
@@ -1,5 +1,7 @@
 package com.devs.tripshare.config.annotations.trips;
 
+import com.devs.tripshare.dto.person.PersonDto;
+import com.devs.tripshare.dto.trip.TripDto;
 import com.devs.tripshare.exceptions.FieldMessage;
 import com.devs.tripshare.exceptions.StandardError;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,7 +19,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Operation(summary = "change a specific trip", description = "This method change a specific trip. Use the trip id.", tags = {"Trip"})
-@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = "application/json")),
+@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ok", content = @Content(schema = @Schema(implementation = TripDto.class), mediaType = "application/json")),
                        @ApiResponse(responseCode = "400", description = "Bad Requests", content = @Content(array = @ArraySchema(schema = @Schema(implementation = FieldMessage.class)), mediaType = "application/json")),
                        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = StandardError.class), mediaType = "application/json"))} )
 public @interface UpdateTripDocConfig {
